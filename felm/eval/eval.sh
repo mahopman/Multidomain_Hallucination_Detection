36d1129a80d2edb0e5fbac21b2554a8ba3f08c97
Mia Hopman <60362144+mahopman@users.noreply.github.com>
2024-07-14 22:54:18 -0400

    Update eval.sh

--- felm/eval/eval.sh
+++ felm/eval/eval.sh
@@ -1,2 +1,2 @@
-python felm/eval/eval.py --path="felm/dataset.jsonl" --model="gpt-3.5-turbo"  --key='sk-proj-2Yk6jmSRb23hyK0vwwMeT3BlbkFJyePwNbsnKrIvgf7RzLKt'  --method='raw'
+python felm/eval/eval.py --path="felm/dataset.jsonl" --model="gpt-3.5-turbo"  --key='...'  --method='raw'
 
