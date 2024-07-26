59f02000fbf1f451f137f8f7a7710ea14cc041cb
robibeyWPI <rfibey@wpi.edu>
2024-07-25 21:40:46 -0400

    BERT, updates

--- framework.py
+++ framework.py
@@ -4,39 +4,90 @@ import felm.eval.eval as felm
 import os
 import time
 import json
+import argparse
+import phase1_bert
+import nltk
+import torch
+from transformers import BertForSequenceClassification, BertTokenizer
+
+BERT_PATH = '.\\bert-classifier\\'
+
+nltk.download('stopwords')
+sw = stopwords.words('english')
 
 
 class Model:
     def __init__(self, dataset: pd.DataFrame) -> None:
         self.dataset = dataset
 
-    def clean_text(self):
-        # taken from phase1_bert.ipynb but could just call something like bert.clean_text() if this is implemented somewhere
-        sw = stopwords.words('english')
+    # def clean_text(self):
+    #     # taken from phase1_bert.ipynb but could just call something like bert.clean_text() if this is implemented somewhere
+    #     sw = stopwords.words('english')
 
+    #     punctuations = '@#!?+&*[]-%.:/();$=><|{}^' + "'`" + '_'
+    #     for p in punctuations:
+    #         self.dataset = self.dataset.replace(p, '')
+
+    #     # Convert words to lower case and remove stop words
+    #     self.dataset = [word.lower()
+    #                     for word in self.dataset.split() if word.lower() not in sw]
+
+    #     return " ".join(self.dataset)
+
+    # def preprocess(self) -> pd.DataFrame:
+    #     '''Takes the input dataframe and adds a cleaned_text column to be used when classifying text. Returns the dataset.'''
+
+    #     self.dataset['cleaned_text'] = self.dataset['text'].apply(
+    #         self.clean_text)
+    #     # or call bert.clean_text()?
+    #     return self.dataset
+
+    def clean_text(text):
+        # Removing punctuations
         punctuations = '@#!?+&*[]-%.:/();$=><|{}^' + "'`" + '_'
         for p in punctuations:
-            self.dataset = self.dataset.replace(p, '')
+            text = text.replace(p, '')
 
         # Convert words to lower case and remove stop words
-        self.dataset = [word.lower()
-                        for word in self.dataset.split() if word.lower() not in sw]
+        text = [word.lower()
+                for word in text.split() if word.lower() not in sw]
 
-        return " ".join(self.dataset)
+        return " ".join(text)
 
-    def preprocess(self) -> pd.DataFrame:
-        '''Takes the input dataframe and adds a cleaned_text column to be used when classifying text. Returns the dataset.'''
+    def preprocess(self):
+        data = json.load(open('./data/eval_data.json'))
 
-        self.dataset['cleaned_text'] = self.dataset['text'].apply(
-            self.clean_text)
-        # or call bert.clean_text()?
-        return self.dataset
+        data = data['prompts']
+        df = pd.DataFrame(data).explode('correct').reset_index()
+        df['text'] = df['question'] + ' ' + df['correct']
+        df = df[['text', 'category']]
+
+        df['cleaned_text'] = df['text'].apply(self.clean_text)
+
+        # convert category to int
+        categories = df['category'].unique()
+        category_map = {category: i for i, category in enumerate(categories)}
+        df['label'] = df['category'].map(category_map)
+
+        return df
 
     def classify_text(self, text: str) -> str:
         '''Takes an input string (ex: prompt) and uses BERT to return a category.'''
+        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        tokenizer = BertTokenizer.from_pretrained(
+            BERT_PATH, local_files_only=True)
+        model = BertForSequenceClassification.from_pretrained(
+            BERT_PATH, local_files_only=True)
+        model.eval()
+        encoding = tokenizer(text, return_tensors='pt',
+                             max_length=512, padding='max_length', truncation=True)
+        input_ids = encoding['input_ids'].to(device)
+        attention_mask = encoding['attention_mask'].to(device)
 
-        category = bert(text) if bert(text) else 'other'  # import bert?
-        return category
+        with torch.no_grad():
+            outputs = model(input_ids=input_ids, attention_mask=attention_mask)
+            _, preds = torch.max(outputs, dim=1)
+        return "positive" if preds.item() == 1 else "negative"
 
     def pass_to_model(self) -> None:
         '''Call BERT to classify into the distinct topic categories as code, wk, st, rw, r, m, or other.
@@ -44,6 +95,8 @@ class Model:
 
         self.dataset['category'] = self.dataset['cleaned_text'].apply(
             self.classify_text)
+
+        results = {}
 
         for index, row in self.dataset.iterrows():
             category = row['category']
@@ -54,6 +107,8 @@ class Model:
             else:
                 result = self.halludetect(row['cleaned_text'])
             # either in the for loop or store results in a list which gets printed later
+            # result can be a tuple such that (actual model result, model used for output purposes)
+            results[result[1]] = results[result[0]]
             self.output_results(result)
 
     def codehalu(self, text: str) -> str:
@@ -73,7 +128,7 @@ class Model:
         result = felm.run(data, model, method, num_cons)
         felm.print_saveresult(data, result, method, model)
 
-        return f'Processed with felm:'
+        return f'Processed with felm, saved to csv'
 
     def halludetect(self, text: str) -> str:
         # call halludetect file
@@ -85,18 +140,31 @@ class Model:
         print(result)
 
 
+def parse_args():
+    parse = argparse.ArgumentParser()
+    parse.add_argument('--path', type=str, help='dataset path')
+
+    args = parse.parse_args()
+    return args
+
+
 if __name__ == '__main__':
-    with open('felm\\dataset.jsonl', 'r', encoding='utf8') as json_file:
-        data = list(json_file)  # csv or json?
-    # data = pd.read_json('felm\\dataset.jsonl', lines=True)
-    # data.to_json('test.json')
-    _json_list = []
-    with open('test.json', 'r', encoding='utf8') as f:
-        data = json.load(f)  # converts to python objects such as dicts
-        for _, top_level in data.items():  # indedx, domain, prompt, labels, etc..
-            for _index, _data in top_level.items():
-                if _index == '0':
-                    _json_list.append(_data)
-    print(_json_list)
+    args = parse_args()
+    path = args.path if args.path else 'data\\eval_data.json'
+
+    # with open(path, 'r', encoding='utf8') as json_file:
+    #     data = list(json_file)
+    with open(path, 'r', encoding='utf8') as json_file:
+        data = json.load(json_file)
+
+    with open('eval_data.jsonl', 'w') as f:
+        for index, prompt in enumerate(data['prompts']):
+            prompt["prompt"] = prompt.pop("question")
+            indexed_prompt = {"index": str(index), **prompt}
+            json.dump(indexed_prompt, f)
+            f.write('\n')
+
+    with open('eval_data.jsonl', 'r', encoding='utf8') as f:
+        data = list(f)
     prompts = Model(data)
     prompts.Felm()
